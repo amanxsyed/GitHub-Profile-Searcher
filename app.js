9a1ef898b81d1628a490cb615b66170263875e74
Umar <umarabdullah.work@gmail.com>
2024-05-14 17:34:13 +0500

    Select all html classes and tags which are used to display information

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 
+// all selectors
+const user_img = document.querySelector(".user-img");
+const userName = document.querySelector(".user-name h2");
+const user_Name = document.querySelector(".user-name h3");
+const followers = document.querySelector(".followers span");
+const following = document.querySelector(".following span");
+const repo_details = document.querySelector(".repo-details");
+const searchBtn = document.querySelector(".btn");
+const visitBtn = document.querySelector(".visit-user a");
